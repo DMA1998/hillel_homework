f67e53d14e8eaba04f25f0ca0084b7345108e734
Dmitriy Mykh <AX5555AC@gmail.com>
2020-07-14 20:48:05 +0300

    added task files

---
+++ HW1/src/Task3.java
@@ -0,0 +1,14 @@
+public class Task3 {
+           public static void main(String[] args) {
+       int[] array = {1,2,3,4,5,6,7};
+
+       for(int arr : array) {
+           System.out.println(arr + " ");
+       }
+
+        for(int arr1 : array) {
+           System.out.print(arr1 + " ");
+       }
+
+        }
+    }
