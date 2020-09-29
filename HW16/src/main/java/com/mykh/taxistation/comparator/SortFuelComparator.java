ef03de2d513007eecabef2989e7ebd3ff07f7bb4
Dmitriy Mykh <AX5555AC@gmail.com>
2020-09-29 21:49:41 +0300

    added homework 16

---
+++ HW16/src/main/java/com/mykh/taxistation/comparator/SortFuelComparator.java
@@ -0,0 +1,13 @@
+package com.mykh.taxistation.comparator;
+
+import com.mykh.taxistation.models.Car;
+
+import java.util.Comparator;
+
+public class SortFuelComparator implements Comparator<Car>{
+
+    @Override
+    public int compare(Car car1, Car car2) {
+        return Double.compare(car1.getFuelConsumption(),car2.getFuelConsumption());
+    }
+}
