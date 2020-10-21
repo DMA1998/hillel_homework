0f2350773368c67b269c349b3491fabffac807ba
Dmitriy Mykh <AX5555AC@gmail.com>
2020-10-21 23:33:58 +0300

    added homework 19

---
+++ HW19/src/test/java/com/mykh/videolib/FilmDaoTest.java
@@ -0,0 +1,11 @@
+import com.mykh.videolib.entities.Film;
+
+import java.util.List;
+
+public class FilmDaoTest {
+
+    public List<Film> findFilmsByCurrentAndPreviousYearTest() {
+        
+        return null;
+    }
+}
