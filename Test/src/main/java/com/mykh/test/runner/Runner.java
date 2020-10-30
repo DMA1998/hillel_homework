ca9bbc17ba517893d5e235eb258a3bf9c743eeb6
Dmitriy Mykh <AX5555AC@gmail.com>
2020-10-30 09:15:23 +0200

    added Runner

---
+++ Test/src/main/java/com/mykh/test/runner/Runner.java
@@ -0,0 +1,13 @@
+package com.mykh.test.runner;
+
+import com.mykh.test.helper.Helper;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Helper helper = new Helper();
+        System.out.println(helper.users());
+
+    }
+
+}
